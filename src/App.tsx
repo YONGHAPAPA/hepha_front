cded6e2aadcdd70c95aad10d2efcb3d7cb4759fc
YONGHAPAPA <zerobreaker@gmail.com>
2020-03-03 13:35:48 +0900

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Task } from './models/task';
+import { NewTaskForm } from './components/NewTaskForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Description from './temp/Description';
+
+
+interface State {
+  newTask: Task;
+  tasks: Task[];
 }
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+
+        <Description countBy={3} />
+      </div>
+    );
+  }
+}
+
 
 export default App;
