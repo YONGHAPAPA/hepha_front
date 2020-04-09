3e71b5282a538bfbdae50a44be33c48b71fdaad6
YONGHAPAPA <zerobreaker@gmail.com>
2020-04-09 14:14:14 +0900

    tree

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,7 +9,7 @@ import {
 const childIds = (childState, action) => {
     switch (action.type) {
         case ADD_CHILD:
-            return [...childState, action.child];
+            return [...childState, action.childId];
         case REMOVE_CHILD:
             return childState.filter(id => id !== action.childId);
         default:
@@ -17,4 +17,59 @@ const childIds = (childState, action) => {
     }
 };
 
-const node = (state, action) => {};
+const node = (state, action) => {
+    switch (action.type) {
+        case CREATE_NODE:
+            return {
+                id: action.nodeId,
+                counter: 0,
+                childIds: []
+            };
+        case INCREMENT:
+            return {
+                ...state,
+                counter: state.counter + 1
+            };
+        case ADD_CHILD:
+        case REMOVE_CHILD:
+            return {
+                ...state,
+                childIds: childIds(state.childIds, action)
+            };
+        default:
+            return state;
+    }
+};
+
+const getAllDescendantIds = (state, nodeId) =>
+    state[nodeId].childIds.reduce(
+        (acc, childId) => [
+            ...acc,
+            childId,
+            ...getAllDescendantIds(state, childId)
+        ],
+        []
+    );
+
+const deleteMany = (state, ids) => {
+    state = { ...state };
+    ids.forEach(id => delete state[id]);
+    return state;
+};
+
+export default (state = {}, action) => {
+    const { nodeId } = action;
+    if (typeof nodeId === "undefined") {
+        return state;
+    }
+
+    if (action.type === DELETE_NODE) {
+        const descendantIds = getAllDescendantIds(state, nodeId);
+        return deleteMany(state, [nodeId, ...descendantIds]);
+    }
+
+    return {
+        ...state,
+        [nodeId]: node(state[nodeId], action)
+    };
+};
